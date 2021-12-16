f0be359575e7b44d91f54b9771b057121624e470
Justin Burks <justin@birdhousebranding.com>
2021-12-16 09:45:03 +0100

    Add webflow styles

--- registration/forms.py
+++ registration/forms.py
@@ -10,3 +10,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = Users
         fields = ["username", "email", "password1", "password2", "affiliation"]
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        # self.fields['username'].widgets.attrs.update({'class', 'field-label'})
+        self.fields['username'].widget.attrs['class'] = 'field-label'
+        self.fields['password1'].widget.attrs['class'] = 'field-label-2'
